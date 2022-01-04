370b2b0b68dbe15d693c7ecd1aa4222c31df8218
jawad <wadja.b@hotmail.fr>
2022-01-04 19:49:01 +0100

    creation et mise forme des vignettes dans la page Home

--- src/components/ListeVignette.jsx
+++ src/components/ListeVignette.jsx
@@ -1,10 +1,14 @@
 import React from "react"
+import Vignette from "../components/Vignette"
 
 class ListeVignette extends React.Component {
   render() {
     return (
       <div className="liste-vignette">
-        
+        <Vignette/>
+        <Vignette/>
+        <Vignette/>
+        <Vignette/>
       </div>
     );
   }
---
+++ src/components/Vignette.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Vignette extends React.Component {
+  render() {
+    return (
+      <div className="vignette">
+        <h2>Titre de la location</h2>
+      </div>
+    );
+  }
+}
+
+export default Vignette;
